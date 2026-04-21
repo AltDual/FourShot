405fe234505ec5d683a8fe9b3768e336b52ea5a9
TheDarkShadow-dev <titanbstar@gmail.com>
2026-04-21 00:11:52 -0700

    added controls screen and changed fonts

--- scripts/title-screen/title_screen.gd
+++ scripts/title-screen/title_screen.gd
@@ -1,14 +1,18 @@
 extends Control
 
 const GAME_SCENE := "res://scenes/game.tscn"
+const CONTROLS_SCENE := "res://scenes/controls.tscn"
 
 @onready var start_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/StartButton
 
 func _ready() -> void:
-    start_button.grab_focus()
+	start_button.grab_focus()
 
 func _on_start_button_pressed() -> void:
-    get_tree().change_scene_to_file(GAME_SCENE)
+	get_tree().change_scene_to_file(GAME_SCENE)
+
+func _on_controls_button_pressed() -> void:
+	get_tree().change_scene_to_file(CONTROLS_SCENE)
 
 func _on_quit_button_pressed() -> void:
-    get_tree().quit()
+	get_tree().quit()
